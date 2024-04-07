50c842fa81d1ea539cba00ee7fb64d51939c4c30
Sam Wray <sam@wray.pro>
2024-04-07 18:17:47 +0100

    fix(list): wrap with clientonly

--- pages/shopping-list.vue
+++ pages/shopping-list.vue
@@ -21,63 +21,65 @@ function filter(ingredients = []) {
 </script>
 
 <template>
-  <r-grid columns="8">
-    <r-cell span="row">
-      <h3><NuxtLink to="/">↩ Home</NuxtLink></h3>
-    </r-cell>
-    <r-cell span="row">
-      <h1>List</h1>
-      <input type="search" v-model="searchTerm" v-if="store.list.length" />
-    </r-cell>
-  </r-grid>
+  <ClientOnly>
+    <r-grid columns="8">
+      <r-cell span="row">
+        <h3><NuxtLink to="/">↩ Home</NuxtLink></h3>
+      </r-cell>
+      <r-cell span="row">
+        <h1>List</h1>
+        <input type="search" v-model="searchTerm" v-if="store.list.length" />
+      </r-cell>
+    </r-grid>
 
-  <r-grid columns="8" v-for="item in store.list">
-    <r-cell span="1-6" span-s="row">
-      <h2>
-        <NuxtLink :to="`/recipe/${item.title}`"> {{ item.title }} →</NuxtLink>
-      </h2>
-    </r-cell>
-    <r-cell span="7-8" span-s="row" class="list-button-container">
-      <button @click="removeFromList(item.title)">
-        <h3>Remove from List</h3>
-      </button>
-    </r-cell>
-    <r-cell span="1-6" span-s="row">
-      <fieldset>
-        <label
-          v-for="ingredient in filter(item.ingredients)"
-          :class="{ checked: ingredient.done }"
-        >
-          <r-grid columns="8">
-            <r-cell span="1-4" class="checkbox-container">
-              <div>
-                <input
-                  type="checkbox"
-                  :id="`${item.title}-${ingredient.name}`"
-                  :checked="ingredient.done"
-                  @change="
-                    check(item.title, ingredient.index, !ingredient.done)
-                  "
-                />
-              </div>
-              <div>
-                <span class="name">{{ ingredient.name }}</span>
-              </div>
-            </r-cell>
-            <r-cell span="5-8">
-              <span class="quantity">{{ ingredient.quantity }}</span>
-            </r-cell>
-          </r-grid>
-        </label>
-      </fieldset>
-    </r-cell>
-  </r-grid>
+    <r-grid columns="8" v-for="item in store.list">
+      <r-cell span="1-6" span-s="row">
+        <h2>
+          <NuxtLink :to="`/recipe/${item.title}`"> {{ item.title }} →</NuxtLink>
+        </h2>
+      </r-cell>
+      <r-cell span="7-8" span-s="row" class="list-button-container">
+        <button @click="removeFromList(item.title)">
+          <h3>Remove from List</h3>
+        </button>
+      </r-cell>
+      <r-cell span="1-6" span-s="row">
+        <fieldset>
+          <label
+            v-for="ingredient in filter(item.ingredients)"
+            :class="{ checked: ingredient.done }"
+          >
+            <r-grid columns="8">
+              <r-cell span="1-4" class="checkbox-container">
+                <div>
+                  <input
+                    type="checkbox"
+                    :id="`${item.title}-${ingredient.name}`"
+                    :checked="ingredient.done"
+                    @change="
+                      check(item.title, ingredient.index, !ingredient.done)
+                    "
+                  />
+                </div>
+                <div>
+                  <span class="name">{{ ingredient.name }}</span>
+                </div>
+              </r-cell>
+              <r-cell span="5-8">
+                <span class="quantity">{{ ingredient.quantity }}</span>
+              </r-cell>
+            </r-grid>
+          </label>
+        </fieldset>
+      </r-cell>
+    </r-grid>
 
-  <r-grid>
-    <r-cell span="row" v-if="!store.list.length">
-      <h2>Nothing in list</h2>
-    </r-cell>
-  </r-grid>
+    <r-grid>
+      <r-cell span="row" v-if="!store.list.length">
+        <h2>Nothing in list</h2>
+      </r-cell>
+    </r-grid>
+  </ClientOnly>
 </template>
 
 <style>
